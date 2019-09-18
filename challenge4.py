17fca4dce8069bc184377e6c7597a07e65cd1fc1
Steph <swils35@acd.ccac.edu>
2019-09-18 18:19:11 -0400

    challenge 4 from first week

---
+++ challenge4.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 13 10:05:31 2019
+
+@author: StephanieWilson
+"""
+
+# loop through two different ranges
+# using nested for loops
+
+list1 = [1, 2, 3, 4]
+list2 = [5, 6, 7]
+
+
+# for i in list1:
+  #  print(str(i) + "| ", end = " ")
+    
+   # for i in list2: 
+    #    print(str(i) + "| ")
